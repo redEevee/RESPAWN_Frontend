69599f8d821aef6ec4851e90bf94145045f51874
Yuyeseul <ryuys0308@naver.com>
2025-08-29 23:37:34 +0900

    Feat: 메인 화면 상품 카테고리별 목록 구현

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
-import ProductContainer from './Product/ProductContainer';
+import CategoryProductSection from '../components/Product/CategoryProductSection';
 
-const MainSection = () => {
-  return (
-    <Section>
-      <ProductContainer limit={9} showMoreButton={true} />
-    </Section>
-  );
-};
+const MainSection = () => (
+  <Container>
+    <CategoryProductSection
+      categoryName="키보드"
+      keywords={['독거미', '특가']}
+      apiCategoryParam="키보드"
+      maxItems={8}
+      gridCols={4}
+    />
+    <CategoryProductSection
+      categoryName="마우스"
+      keywords={['갤럭시', '혜택', '사은품']}
+      apiCategoryParam="마우스"
+      maxItems={8}
+      gridCols={4}
+    />
+    <CategoryProductSection
+      categoryName="모니터"
+      keywords={['게이밍', 'IPS', '고주사율']}
+      apiCategoryParam="모니터"
+      maxItems={8}
+      gridCols={4}
+    />
+    <CategoryProductSection
+      categoryName="본체"
+      keywords={['조립PC', '라이젠', '특가']}
+      apiCategoryParam="본체"
+      maxItems={8}
+      gridCols={4}
+    />
+    <CategoryProductSection
+      categoryName="스피커"
+      keywords={['블루투스', '2.1채널', '가성비']}
+      apiCategoryParam="스피커"
+      maxItems={8}
+      gridCols={4}
+    />
+  </Container>
+);
 
 export default MainSection;
 
-const Section = styled.section`
-  width: 100%;
-  max-width: 1200px;
+const Container = styled.div`
+  max-width: 1320px;
   margin: 40px auto;
   padding: 0 20px;
 `;
